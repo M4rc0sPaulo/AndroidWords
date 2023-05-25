c851e60ab9eec6f73237165d203fd0834ede54dd
Marcos <marcospborborema@hotmail.com>
2023-05-24 21:40:03 -0300

    Começo

---
+++ app/src/main/java/com/example/words/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.words
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.word.LetterAdapter
+import com.example.words.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+}
